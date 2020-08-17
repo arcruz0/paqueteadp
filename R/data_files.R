94b1e776d3a0f99a692dae9c59b86122446d1f35
arcruz0 <arcruz@uc.cl>
2020-08-16 22:50:46 -0400

    Versión 1.0 paquete

--- R/data_files.R
+++ R/data_files.R
@@ -1,38 +1,51 @@
+#' Aprobación presidencial
+#'
+#' Datos dprobación presidencial por trimestre, 2000-2014, compilada por Catherine Reyes-Housholder. Se suman algunas variables del proyecto Quality of Government (2017).
+#'
+#' @name aprobacion
+#' @docType data
+#' @references Reyes-Housholder, C. (2019). A Theory of Gender’s Role on Presidential Approval Ratings in Corrupt Times. Political Research Quarterly.
+#' @references \url{https://qog.pol.gu.se/data}
+#' @keywords data
+NULL
+
+#' Aprobación presidencial (wide)
+#'
+#' Datos de aprobación presidencial por trimestre, 2000-2014, compilada por Catherine Reyes-Housholder. Se suman algunas variables del proyecto Quality of Government (2017). Formato wide.
+#'
+#' @name aprobacion_wide1
+#' @docType data
+#' @references Reyes-Housholder, C. (2019). A Theory of Gender’s Role on Presidential Approval Ratings in Corrupt Times. Political Research Quarterly.
+#' @references \url{https://qog.pol.gu.se/data}
+#' @keywords data
+NULL
+
+#' Aprobación presidencial (wide 2)
+#'
+#' Datos de aprobación presidencial por trimestre, 2000-2014, compilada por Catherine Reyes-Housholder. Se suman algunas variables del proyecto Quality of Government (2017). Formato wide (2).
+#'
+#' @name aprobacion_wide2
+#' @docType data
+#' @references Reyes-Housholder, C. (2019). A Theory of Gender’s Role on Presidential Approval Ratings in Corrupt Times. Political Research Quarterly.
+#' @references \url{https://qog.pol.gu.se/data}
+#' @keywords data
+NULL
+
 #' Bienestar en Latinoamérica
 #'
-#' @name bienestar_la
+#' Datos de bienestar en Latinoamérica, por Huber & Stephens (2012)
+#'
+#' @name bienestar
 #' @docType data
 #' @references \url{http://huberandstephens.web.unc.edu/common-works/data/}
 #' @keywords data
 NULL
 
-#' Índice de capacidades materiales nacionales, de Correlates of War (CoW). Versión 5
+#' Copatrocinio en el Senado argentino (1983)
 #'
-#' @name cinc_cow
-#' @docType data
-#' @references Singer, J. David, Stuart Bremer, and John Stuckey. (1972). "Capability Distribution, Uncertainty, and Major Power War, 1820-1965." in Bruce Russett (ed) Peace, War, and Numbers, Beverly Hills: Sage, 19-48.
-#' @keywords data
-NULL
-
-#' Democracia directa
+#' Datos de presentación conjunta de proyectos de ley en el Senado argentino
 #'
-#' @name dem_directa
-#' @docType data
-#' @references Altman, D. (2018). Citizenship and Contemporary Direct Democracy. Cambridge University Press.
-#' @keywords data
-NULL
-
-#' Encuesta LAPOP
-#'
-#' @name lapop
-#' @docType data
-#' @references Altman, D. (2018). Citizenship and Contemporary Direct Democracy. Cambridge University Press.
-#' @keywords data
-NULL
-
-#' Presentación de proyectos de ley en el Senado argentino (1983)
-#'
-#' @name leyes_argentina
+#' @name copatrocinio_arg
 #' @docType data
 #' @references Alemán, E., Calvo, E., Jones, M. P., & Kaplan, N. (2009). Comparing Cosponsorship and Roll‐Call Ideal Points. Legislative Studies Quarterly, 34(1), 87-116.
 #' @keywords data
@@ -40,96 +53,147 @@ NULL
 
 #' Datos de municipios chilenos (1992-2012)
 #'
-#' Base de datos obtenida por Soledad Araya.
+#' Base de datos obtenida por Soledad Araya, con variables para municipios chilenos entre 1992 y 2012.
 #'
-#' @name municipios_chile
+#' @name datos_municipales
 #' @docType data
 #' @keywords data
 NULL
 
-#' Datos de países del mundo, por Quality of Government (2017)
+#' Democracia directa
 #'
-#' @name qog
+#' Datos de instituciones de democracia directa en el mundo, compilados por David Altman (2018).
+#'
+#' @name democracia_directa
 #' @docType data
-#' @references \url{https://qog.pol.gu.se/data}
+#' @references Altman, D. (2018). Citizenship and Contemporary Direct Democracy. Cambridge University Press.
+#' @keywords data
+NULL
+
+
+#' Edad y educación como confounders
+#'
+#' Datos simulados, en los que la edad y la educación son confounders para el tratamiento de usar redes de mosquitos. Creados por Andrew Heiss.
+#'
+#' @name edad_educ
+#' @docType data
+#' @keywords data
+NULL
+
+#' Proximidad política entre EEUU y Brasil
+#'
+#' Datos de proximidad política entre EEUU y Brasil, generados a partir de sus votaciones en la Asamblea General de las Naciones Unidas. Datos originales de Voeten (2012).
+#'
+#' @name eeuu_brasil
+#' @docType data
+#' @references Voeten, E. (2012). Data and analyses of voting in the un general assembly (SSRN Scholarly Paper ID 2111149). Social Science Research Network. \url{https://papers.ssrn.com/abstract=2111149}
+#' @keywords data
+NULL
+
+#' Proximidad política entre EEUU y países de Latinoamérica
+#'
+#' Datos de proximidad política entre EEUU y países de Latinoamérica, generados a partir de sus votaciones en la Asamblea General de las Naciones Unidas. Datos originales de Voeten (2012).
+#'
+#' @name eeuu_latam
+#' @docType data
+#' @references Voeten, E. (2012). Data and analyses of voting in the un general assembly (SSRN Scholarly Paper ID 2111149). Social Science Research Network. \url{https://papers.ssrn.com/abstract=2111149}
+#' @keywords data
+NULL
+
+#' Datos de los estados de Brasil
+#'
+#' Datos de los estados de Brasil, compilados por Freire (2018)
+#'
+#' @name edad_educ
+#' @docType data
+#' @references Freire, D. (2018). Evaluating the effect of homicide prevention strategies in são paulo, brazil: A synthetic control approach. Latin American Research Review, 53(2), 231. \url{https://doi.org/10.25222/larr.334}
+#' @keywords data
+NULL
+
+#' Índice de capacidades materiales nacionales, de Correlates of War (CoW). Versión 5.
+#'
+#' @name indice_cinc
+#' @docType data
+#' @references Singer, J. David, Stuart Bremer, and John Stuckey. (1972). "Capability Distribution, Uncertainty, and Major Power War, 1820-1965." in Bruce Russett (ed) Peace, War, and Numbers, Beverly Hills: Sage, 19-48.
+#' @keywords data
+NULL
+
+#' Encuesta LAPOP
+#'
+#' Datos de la encuesta LAPOP (2016)
+#'
+#' @name lapop
+#' @docType data
+#' @references \url{https://www.vanderbilt.edu/lapop/ab2016/AB2017-v18.0-Spa-170202_W.pdf}
+#' @keywords data
+NULL
+
+#' PIB per capita PPP en América
+#'
+#' Datos de PIB per capita con paridad de poder adquisitivo para los países de América, 1996-1998. Datos de los World Development Indicators del Banco Mundial.
+#'
+#' @name pib_pc_america
+#' @docType data
+#' @references \url{https://data.worldbank.org/indicator/NY.GDP.PCAP.PP.CD}
+#' @keywords data
+NULL
+
+#' Tweets políticos en Chile
+#'
+#' Tweets de diputados(as) chilenos(as), entre abril y junio de 2018. Compilados por Sebastián Huneeus.
+#'
+#' @name poltweets
+#' @docType data
+#' @references \url{https://www.vanderbilt.edu/lapop/ab2016/AB2017-v18.0-Spa-170202_W.pdf}
 #' @keywords data
 NULL
 
 #' Quiebres democráticos en Latinoamérica
 #'
-#' @name quiebres_dem_la
+#' Datos de quiebres democráticos en América Latina, 1900-2010, por Mainwaring & Pérez-Liñán (2013).
+#'
+#' @name quiebre_democracia
 #' @docType data
 #' @references Mainwaring, S., & Pérez-Liñán, A. (2013). Democracies and dictatorships in Latin America: emergence, survival, and fall. Cambridge University Press.
 #' @keywords data
 NULL
 
+#' Redes de mosquitos
+#'
+#' Datos simulados, en los que se evalúa el tratamiento de redes de mosquitos en el riesgo de malaria. Creados por Andrew Heiss.
+#'
+#' @name red_mosquitos
+#' @docType data
+#' @keywords data
+NULL
+
+#' Resumen desordenado del PIB per capita de países americanos
+#'
+#' Resumen "desordenado" del PIB per capita de países americanos, en el que los nombres de los países no siguen un estándar. Dato del PIB per capita desde los World Development Indicators del Banco Mundial.
+#'
+#' @name resumen_desordenado
+#' @docType data
+#' @references \url{https://data.worldbank.org/indicator/NY.GDP.PCAP.PP.CD}
+#' @keywords data
+NULL
+
+#' Senadores de Argentina (1983)
+#'
+#' Datos de senadores argentinos(as), 1983.
+#'
+#' @name senadores_arg
+#' @docType data
+#' @references Alemán, E., Calvo, E., Jones, M. P., & Kaplan, N. (2009). Comparing Cosponsorship and Roll‐Call Ideal Points. Legislative Studies Quarterly, 34(1), 87-116.
+#' @keywords data
+NULL
+
 #' Tratados internacionales
 #'
-#' Base de datos obtenida por Carsten Schulz.
+#' Datos de adopción, ratificación, etc. de tratados internacionales. Compilados por Carsten Schulz en base al repositorio de las Naciones Unidas.
 #'
-#' @name tratados_int
+#' @name tratados
 #' @docType data
-#' @keywords data
-NULL
-
-#' Tweets de diputados/as de Chile (2018)
-#'
-#' Base de datos obtenida por Sebastián Huneeus, que contiene los tweets de los 155 diputados/as de Chile entre el 1 de mayo y el 10 de junio de 2018.
-#'
-#' @name tratados_int
-#' @docType data
-#' @keywords data
-NULL
-
-#' Crimen en Brasil
-#'
-#' Base de datos de crimen en los estados de Brasil.
-#'
-#' @name crimen_brasil
-#' @docType data
-#' @references Freire, D. (2018). Evaluating the effect of homicide prevention strategies in Sao Paulo, Brazil: A synthetic control approach. Latin American Research Review, 53(2).
-#' @keywords data
-NULL
-
-#' Discursos de Allende
-#'
-#' discursos públicos de Salvador Allende entre finales de 1969 y el 11 de septiembre de 1973.
-#'
-#' @name discursos_allende
-#' @docType data
-#' @references \url{https://www.marxists.org/espanol/allende/index.htm}
-#' @keywords data
-NULL
-
-#' Aprobación presidencial
-#'
-#' Aprobación presidencial por trimestre, 2000-2014, compilada por Catherine Reyes-Housholder. Se suman algunas variables del proyecto Quality of Government (2017).
-#'
-#' @name aprob_presidencial
-#' @docType data
-#' @references Reyes-Housholder, C. (Forthcoming). A Theory of Gender’s Role on Presidential Approval Ratings in Corrupt Times. Political Research Quarterly.
-#' @references \url{https://qog.pol.gu.se/data}
-#' @keywords data
-NULL
-
-#' Aprobación presidencial (wide)
-#'
-#' Aprobación presidencial por trimestre, 2000-2014, compilada por Catherine Reyes-Housholder. Se suman algunas variables del proyecto Quality of Government (2017). Formato wide.
-#'
-#' @name aprob_presidencial_wide
-#' @docType data
-#' @references Reyes-Housholder, C. (Forthcoming). A Theory of Gender’s Role on Presidential Approval Ratings in Corrupt Times. Political Research Quarterly.
-#' @references \url{https://qog.pol.gu.se/data}
-#' @keywords data
-NULL
-
-#' Aprobación presidencial (wide 2)
-#'
-#' Aprobación presidencial por trimestre, 2000-2014, compilada por Catherine Reyes-Housholder. Se suman algunas variables del proyecto Quality of Government (2017). Formato wide (2).
-#'
-#' @name aprob_presidencial_wide2
-#' @docType data
-#' @references Reyes-Housholder, C. (Forthcoming). A Theory of Gender’s Role on Presidential Approval Ratings in Corrupt Times. Political Research Quarterly.
-#' @references \url{https://qog.pol.gu.se/data}
+#' @references \url{https://www.carstenschulz.eu/}
+#' @references \url{https://tratados.un.org/}
 #' @keywords data
 NULL
